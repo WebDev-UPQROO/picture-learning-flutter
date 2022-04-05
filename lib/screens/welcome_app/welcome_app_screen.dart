14bb2034d14499c391366ee2f095628a4aee4a2c
Erick Espinoza <erickesp2077@gmail.com>
2022-04-05 10:11:07 -0500

    welcomeApp (fixed buttons and screen)

--- lib/screens/welcome_app/welcome_app_screen.dart
+++ lib/screens/welcome_app/welcome_app_screen.dart
@@ -2,35 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:picture_learning/constants/style.dart';
 import 'package:picture_learning/widgets/gaps/gap_04.dart';
 import 'package:picture_learning/widgets/painters/painer_curve.dart';
+
+import '../../routes.dart';
 //for storing form state.
 class WelcomeAppScreen extends StatelessWidget {
-  const WelcomeAppScreen({Key? key}) : super(key: key);
+  PageController _pageController = PageController();
+
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    
 
     return Scaffold(
       body: PageView(
-        children: <Widget>[
+        controller: _pageController,
+        children: [
           WelcomePages(
              imgOnTop: Image.asset('assets/img/welcome_man.png', height: size.height * 0.43,),
              onRight: 0.15,
              titleText: 'Bienvenido',
              descText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus tempus diam non varius. Aenean imperdiet consectetur lorem.',
-
+             onPressButton: (){
+               _pageController.animateToPage(_pageController.page!.toInt() + 1,
+                duration: Duration(milliseconds: 400),
+                curve: Curves.easeIn);
+             },
           ),
           WelcomePages(
              imgOnTop: Image.asset('assets/img/welcome_icon.png', height: size.height * 0.47,),
              onRight: 0.25,
              titleText: 'Picture Learning',
              descText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus tempus diam non varius. Aenean imperdiet consectetur lorem.',
-
+             onPressButton: (){
+               Navigator.pushNamed(context, Routes.loginOAuth);
+             },
           )
+          
         ]
-      )
+      ),
+
     );
-  }
+
+  } 
 }
 
 class WelcomePages extends StatelessWidget {
@@ -41,23 +55,26 @@ class WelcomePages extends StatelessWidget {
     required this.onRight,
     required this.titleText,
     required this.descText,
+    required this.onPressButton,
   }) 
   : super(key: key);
 
   final Widget imgOnTop;
-   final double onRight;
+  final double onRight;
   final String titleText;
   final String descText;
+  final Function() onPressButton;
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     
-    return Scaffold(
+   return Scaffold(
       backgroundColor: Style.primary,
       body: Stack(
         alignment: Alignment.bottomCenter,
         children: [
+          
           Align(
             alignment: Alignment.bottomCenter,
             child: SizedBox(
@@ -68,6 +85,29 @@ class WelcomePages extends StatelessWidget {
               ),
             ),
           ),
+         
+           Positioned(
+            right: size.width * 0.07,
+            top: size.height * 0.02,   
+            child:
+           SizedBox(
+            height: 36.0,
+            width: 36.0,
+            child: IconButton(
+              onPressed: (){
+                Navigator.pushNamed(context, Routes.loginOAuth);
+              }, 
+              icon: const Icon(
+                Icons.highlight_off,
+                size: 36.0,
+                color: Colors.white
+                ),
+              )
+          ),
+             
+              
+             
+          ), 
           Positioned(
             right: size.width * onRight,
             top: size.height * 0.08,
@@ -101,13 +141,10 @@ class WelcomePages extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: size.height * 0.01),
-                  Directionality(
-                    
+                  Directionality(                   
                     textDirection: TextDirection.rtl,
                     child: ElevatedButton.icon(
-                    onPressed: () {
-                            
-                    },
+                    onPressed: onPressButton,
                     
                     icon: const Icon(Icons.chevron_left),
                     label: const Text('Siguiente'),
@@ -115,13 +152,13 @@ class WelcomePages extends StatelessWidget {
                     ),
                     
                   ),
-
                 ],
               ),
             ),
           )
         ],
       ),
-    );
+    ); 
   }
+    
 }
